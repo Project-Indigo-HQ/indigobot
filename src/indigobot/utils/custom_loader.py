6b00444b1a7773309def396b99b5eee487a948d7
Kyle Klein <132229861+kk60503@users.noreply.github.com>
2025-03-02 11:08:08 -0800

    Update custom_loader.py

--- src/indigobot/utils/custom_loader.py
+++ src/indigobot/utils/custom_loader.py
@@ -26,11 +26,8 @@ from indigobot.utils.jf_crawler import crawl
 from indigobot.utils.redundancy_check import check_duplicate
 from indigobot.utils.refine_html import load_JSON_files, refine_text
 
-# chunk_size = 512
-# chunk_overlap = 10
-
-chunk_size = 10000
-chunk_overlap = 1000
+chunk_size = 512
+chunk_overlap = 10
 
 
 def clean_text(text):
